3690eae2c45464e7c8ad62a71c4cea0e80d860fc
PeopleAnalyticTeam <124548434+PeopleAnalyticTeam@users.noreply.github.com>
2023-03-16 14:55:39 +0700

    Add files via upload

--- streamlit_prophet/lib/inputs/dataset.py
+++ streamlit_prophet/lib/inputs/dataset.py
@@ -9,7 +9,7 @@ from streamlit_prophet.lib.utils.load import download_toy_dataset, load_custom_c
 def input_dataset(
     config: Dict[Any, Any], readme: Dict[Any, Any], instructions: Dict[Any, Any]
 ) -> Tuple[pd.DataFrame, Dict[Any, Any], Dict[Any, Any], Dict[Any, Any]]:
-    """Lets the user decide whether to upload a dataset or download a toy dataset.
+    """Silahkan upload dataset deret waktu yang akan dianalisa. Lets the user decide whether to upload a dataset or download a toy dataset.
 
     Parameters
     ----------
@@ -33,11 +33,11 @@ def input_dataset(
     """
     load_options, datasets = dict(), dict()
     load_options["toy_dataset"] = st.checkbox(
-        "Load a toy dataset", True, help=readme["tooltips"]["upload_choice"]
+        "Gunakan Data Sampel", False, help=readme["tooltips"]["upload_choice"]
     )
     if load_options["toy_dataset"]:
         dataset_name = st.selectbox(
-            "Select a toy dataset",
+            "Pilih sampel dataset",
             options=list(config["datasets"].keys()),
             format_func=lambda x: config["datasets"][x]["name"],
             help=readme["tooltips"]["toy_dataset"],
